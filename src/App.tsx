66c11b48a4dbd0e8c8439a6730174d898e7e0f0f
Jose Rubio <jadrubio@gmail.com>
2024-04-15 18:34:12 -0400

    carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Navbar } from "./layouts/Navbar";
+import { TopBooks } from "./components/TopBooks";
+import { Carousel } from "./components/Carousel";
 import "./App.css";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <TopBooks />
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
