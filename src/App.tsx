22578e73ab2bf8318492fbde3a9f7e1194b372ea
Jose Rubio <jadrubio@gmail.com>
2024-04-16 10:46:22 -0400

    home page layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Navbar } from "./layouts/Navbar";
 import { TopBooks } from "./components/TopBooks";
 import { Carousel } from "./components/Carousel";
 import "./App.css";
+import { HomePage } from "./layouts/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <TopBooks />
-      <Carousel />
+      <HomePage />
     </>
   );
 }
---
+++ src/layouts/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Carousel } from "../components/Carousel";
+import { TopBooks } from "../components/TopBooks";
+
+export const HomePage = () => {
+  return (
+    <>
+      <TopBooks />
+      <Carousel />
+    </>
+  );
+};
